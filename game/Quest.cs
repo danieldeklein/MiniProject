4547a3297e22e9cab2c0e7a12891df73e6fb058a
ErikTanis <eriktanis21@gmail.com>
2024-02-27 13:27:46 +0100

    Added quest

---
+++ game/Quest.cs
@@ -0,0 +1,15 @@
+class Quest 
+{
+
+    public string Description;
+    public string ID;
+    public string Name;
+
+    public Quest(string Name, string ID, string Description)
+    {
+        this.Name = Name;
+        this.ID = ID;
+        this.Description = Description;
+    }
+
+}
