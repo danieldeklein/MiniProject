0cade552820933831bad0d01fd8060dfaba9c5ff
Lijcoesh <145970950+Lijcoesh@users.noreply.github.com>
2024-02-27 15:22:24 +0100

    Add files via upload

--- game/Program.cs
+++ game/Program.cs
@@ -6,7 +6,6 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        // Create the map
         string[,] map = new string[4, 6]
         {
             { " ", " ", "P", " ", " ", " "},
@@ -15,11 +14,9 @@ class Program
             { " ", " ", "H", " ", " ", " "}
         };
 
-        // Set the player's starting position
         int playerX = 3;
         int playerY = 6;
 
-        // Print the map
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 6; j++)
@@ -36,11 +33,9 @@ class Program
             Console.WriteLine();
         }
 
-        // Get user input for movement
         Console.WriteLine("Choose a direction to move (up, down, left, right):");
-        string direction = Console.ReadLine();
+        string? direction = Console.ReadLine();
 
-        // Update player's position based on user input
         if (direction == "up" && playerX > 0)
         {
             playerX--;
@@ -58,7 +53,6 @@ class Program
             playerY++;
         }
 
-        // Print the updated map with player's new position
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 7; j++)
@@ -74,23 +68,6 @@ class Program
             }
             Console.WriteLine();
         }
-        if (direction == "up" && playerX > 0)
-        {
-            playerX--;
-        }
-        else if (direction == "down" && playerX < 3) // Adjusted condition to playerX < 3
-        {
-            playerX++;
-        }
-        else if (direction == "left" && playerY > 0)
-        {
-            playerY--;
-        }
-        else if (direction == "right" && playerY < 5) // Adjusted condition to playerY < 5
-        {
-            playerY++;
-        }
-
     }
     
 }
