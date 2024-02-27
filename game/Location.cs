783e049f0b95d52987823e1534a7804eb12ab2b0
ErikTanis <eriktanis21@gmail.com>
2024-02-27 13:44:50 +0100

    -

--- game/Location.cs
+++ game/Location.cs
@@ -4,12 +4,17 @@ class Location
     public int ID;
     public string Name;
     public string Description;
+    public Quest? QuestAvailableHere;
+    public Monster? MonsterLivingHere;
 
-    public Location(int ID, string Name, string Description, string? x, string? y)
+
+    public Location(int ID, string Name, string Description, Quest quest, Monster monster)
     {
         this.ID = ID;
         this.Name = Name;
         this.Description = Description;
+        QuestAvailableHere = quest;
+        MonsterLivingHere = monster;
     }
 
 }
