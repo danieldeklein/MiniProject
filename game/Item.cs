0e612dee591520e6d9b98c29ef82577a7b5cb5e6
ErikTanis <eriktanis21@gmail.com>
2024-02-28 16:23:42 +0100

    Added option to add use function on item

--- game/Item.cs
+++ game/Item.cs
@@ -4,12 +4,30 @@ public class Item
     public string Name;
     public int Amount;
     public int ID;
+    public Func<string>? Use;
 
-    public Item(int ID, string Name, int Amount)
+    public Item(int ID, int Amount)
     {
+        Name = GetName(ID);
         this.ID = ID;
-        this.Name = Name;
         this.Amount = Amount;
+    }
+
+    private string GetName(int ID)
+    {
+        switch (ID)
+        {
+            case 1:
+                return "Health Potion";
+            case 2:
+                return "Rat Tail";
+            case 3:
+                return "Snake Skin";
+            case 4:
+                return "Spider Silk";
+            default:
+                return "Unknown";
+        }
     }
 
 }
